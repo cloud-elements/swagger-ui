9a1fa95a065e793e9b9aebb6e15e13c551ef2fed
Martin Brandt <martin@cloud-elements.com>
2017-02-13 13:52:24 -0700

    proper encoding of single quotes in generated cURL

--- dist/swagger-ui.js
+++ dist/swagger-ui.js
@@ -5230,8 +5230,13 @@ Operation.prototype.asCurl = function (args1, args2) {
     results.push('-d \'' + body.replace(/\'/g, '\\u0027') + '\'');
   }
 
-  return 'curl ' + (results.join(' ')) + ' \'' + obj.url + '\'';
+   return 'curl ' + (results.join(' ')) + ' \'' + this.formatURLForCurl(obj.url) + '\'';
 };
+
+Operation.prototype.formatURLForCurl = function(url) {
+    return url.replace(/\'/g, '%27');
+};
+
 
 Operation.prototype.encodePathCollection = function (type, name, value) {
   var encoded = '';
